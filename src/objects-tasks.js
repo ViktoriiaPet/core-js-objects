161d71c3dceeb31133f83ae299dc7dc7e2343687
Viktoriia Pet <viktoriiap0205199@gmail.com>
2025-03-01 16:01:49 +0100

    feat: add tasks 6 and 7

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -159,8 +159,31 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const bills = { 25: 0, 50: 0 };
+
+  return queue.every((bill) => {
+    if (bill === 25) {
+      bills[25] += 1;
+    } else if (bill === 50) {
+      if (bills[25] > 0) {
+        bills[25] -= 1;
+        bills[50] += 1;
+      } else {
+        return false;
+      }
+    } else if (bill === 100) {
+      if (bills[50] > 0 && bills[25] > 0) {
+        bills[50] -= 1;
+        bills[25] -= 1;
+      } else if (bills[25] >= 3) {
+        bills[25] -= 3;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  });
 }
 
 /**
@@ -176,8 +199,15 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const object = {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
+  return object;
 }
 
 /**
